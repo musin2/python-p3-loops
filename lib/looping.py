1dfede08230b2d61b08703cb035eba37d19910f8
musin2 <muhsindaud75@gmail.com>
2024-08-27 14:54:13 +0300

    adds lab solution

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,34 @@
 #!/usr/bin/env python3
-
+import ipdb
 def happy_new_year():
-    # code goes here!
-    pass
+    i=10
+    while (i>= 0):
+        if i != 0:
+           print(i) 
+        else:
+            print("Happy New Year!")
+        i-=1
+
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    # return [intgr * intgr for intgr in int_list]
+    new_list= list()
+    for intgr in int_list:
+        res=intgr * intgr
+        new_list.append(res)
+    return new_list
+
 
 def fizzbuzz():
-    # code goes here!
-    pass
+   i=1
+   while(i<=100):
+       if i % 3 == 0 and i % 5 != 0:
+           print("Fizz")
+       elif i % 3 != 0 and i % 5 == 0:
+           print('Buzz')
+       elif i % 3 == 0 and i % 5 == 0:
+           print('FizzBuzz')
+       else:
+           print(i)
+       i+=1
+
